5283bdb2959083529e9d283fd26fa64f052b1074
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-14 11:24:51 +0300

    Add files via upload

---
+++ Beginnig of INHERITANCE/Hayvan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginnig_of_INHERITANCE
+{
+    public class Hayvan
+    {
+        public string AnimalName;
+        public string AnimalGender;
+        public string AnimalColor;
+        public int AnimalAge;
+
+
+        public void ShowAnimalInfo()
+        {
+            Console.WriteLine("Animal's name is "+ "[" + AnimalName + "]") ;
+            Console.WriteLine("Animal's gender is "+ "[" + AnimalGender + "]");
+            Console.WriteLine("Animal's color is " + "[" + AnimalColor + "]");
+            Console.WriteLine("Animal's Age is " + "[" + AnimalAge + "]");
+        }
+
+    }
+}
---
+++ Beginnig of INHERITANCE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginnig_of_INHERITANCE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat kedi = new Cat();
+
+            kedi.AnimalName = "Boncuk";
+            kedi.AnimalGender = "Female";
+            kedi.AnimalColor = "White";
+            kedi.AnimalAge = 2; 
+            kedi.ShowAnimalInfo();
+
+
+            Console.ReadLine();
+        }
+    }
+}
