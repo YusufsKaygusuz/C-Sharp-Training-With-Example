1adc7b21848fac9c7c17406daa39f3bb22f3e57a
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-14 11:06:44 +0300

    Uploaded the Access Specifiers

---
+++ Access Specifiers(Erişim Belirleyicileri)/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Specifiers_Erişim_Belirleyicileri_
+{
+    class Customer // We have set (adjusted) the access identifier to be "public".
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                if(string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("name is not null");
+                _name = value;
+            }
+        }
+
+        public string Surname;
+        public float Salary;
+        public string Gender;
+        public string DateofBirth;
+
+        private double ID = 458741365; // The private specifier can only be used in the class in which it is defined. That is, it is closed
+                           // to the outside world.
+
+        public void DemonstrateCusInfo()
+        {
+            Console.WriteLine("Name: "+ Name);
+            Console.WriteLine("Surname: "+ Surname);
+            Console.WriteLine("Salary: "+ Salary);
+            Console.WriteLine("Gender: "+ Gender);
+            Console.WriteLine("Date of Brith: "+ DateofBirth);
+            Console.WriteLine("Identity: " +ID);
+        }
+
+        private void Name_Surname(string CustomerName, string CustomerSurname)
+        {
+            Console.WriteLine("Customer's name and surname: " +CustomerName +" " + CustomerSurname);
+        }
+
+
+    }
+}
---
+++ Access Specifiers(Erişim Belirleyicileri)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Specifiers_Erişim_Belirleyicileri_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*public --- *private --- protected --- internal
+
+            Customer client = new Customer();
+
+            client.Name = "Begüm";
+            client.Surname = "Yıldırımer";
+            client.Gender = "Woman";
+            client.DateofBirth = "12/6/2000";
+            client.Salary = 2146.01f;
+            client.DemonstrateCusInfo();
+
+            Customer client2 = new Customer();
+            client2.Name = "NULL"; // If client2.Name is null or empty, system throw error.
+            client2.Surname = "mike";
+            client2.DemonstrateCusInfo();
+
+            Console.ReadLine();
+
+        }
+    }
+}
