bb028679b7a8ecd4cdc28b33d59b3443b6601701
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-14 11:30:03 +0300

    Add files via upload

---
+++ Register Student/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Kayıt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("Eslem Naz Yanik", 202015463);
+            Console.WriteLine(stu.InfoStudent());
+
+            Student stu2 = new Student("Aslıhan Çelik", 20210510, "aslihancelix@gmail.com", 2.69);
+            Console.WriteLine("\n" + stu2.InfoStudent());
+
+            Student stu3 = new Student("Bora Çakır", 201101001, "bora@gmail.com", 3.36);
+            Console.WriteLine("\n" + stu3.InfoStudent());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Register Student/Student.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Kayıt
+{
+    class Student
+    {
+        // İsim ve soyisim özelliği
+        private string _nameSurname;
+        public string NameSurname
+        {
+            get { return _nameSurname; }
+            /*
+             nameof operatörü, belirtilen bir değişkenin veya özellik adının, string bir ifade olarak geri döndürülmesini sağlar.
+             Bu operatör, kodda yapılan değişikliklerin, kodun diğer kısımlarına etkisi olmadan güncellenmesine olanak tanır.
+             */
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException(nameof(NameSurname), "İsim ve soyisim alanı boş bırakılamaz");
+                
+                _nameSurname = value;
+            }
+        }
+
+        // Numara özelliği
+        private double _numara;
+
+        public double Numara
+        {
+            get { return _numara; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentNullException(nameof(Numara), "Numara sıfırdan büyük olmalı.");
+
+                _numara = value;
+            }
+        }
+
+        // Ortalama özelliği
+        private double _average;
+        public double Average
+        {
+            get { return _average; }
+
+            set {
+                if (value < 0.0 || value > 4.0)
+                    throw new ArgumentNullException(nameof(Average), "Ortalama 0.0 ve 4.0 aralığında olmalı.");
+                _average = value;
+            }
+        }
+
+
+
+        private string Email;
+        public string email {
+            get
+            {
+                return Email;
+            }
+            set
+            {
+                var mailAdress = new System.Net.Mail.MailAddress(value);
+                if(mailAdress.Address == value)
+                {
+                    Email = value;
+                }
+                else
+                {
+                    throw new Exception("Geçerli bir email adresi girilmedi.");
+                }
+            }
+  
+           }
+       
+        #region Constructors
+        public Student(string isimSoyi, int numara)
+        {
+            Numara = numara;
+            NameSurname = isimSoyi;
+            this.Average = 0.0;
+            this.Email = isimSoyi.Replace(" ", string.Empty) + "@hotmail.com";
+        }
+        public Student(string isimSoyisim,int numara, string EMAİL, double ortalama)
+            :this(isimSoyisim,numara)
+        {
+            this.email = EMAİL;
+            this.Average = ortalama;
+        }
+
+        public string InfoStudent()
+        {
+            return $"İsim Soyisim: {NameSurname}\nNumara: {Numara}\nOrtalama: {Average}\nEmail: {email}"; 
+        }
+        #endregion
+
+    }
+}
